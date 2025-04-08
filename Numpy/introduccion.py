ddbe7036ae77eb1109f287e4188ca00c24a36d1f
Daniel <litodanielsolid@gmail.com>
2025-04-07 22:13:31 -0600

    Introduccion a numpy

---
+++ Numpy/introduccion.py
@@ -0,0 +1,9 @@
+import numpy as np
+# Numpy es una libreria de Python que permite trabajar con arreglos multidimensionales y matrices, ademas de contar con funciones matematicas de alto nivel.
+
+
+array = np.array([1, 2, 3, 4, 5])
+print(array)
+# El objeto array es un arreglo de una dimension, pero se pueden crear arreglos de dos o mas dimensiones.
+array2 = np.array([[1, 2, 3], [4, 5, 6]])   
+print(array2)
